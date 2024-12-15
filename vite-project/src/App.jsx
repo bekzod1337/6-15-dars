04e1f55de51e0ef5917bbe40874d76191014c90a
bekzod1337 <rahimjonovazarina@gmail.com>
2024-12-15 10:04:49 +0500

    third commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Main from "./components/Index"; 
+import Main from "./Components/Index1"; 
 import CreateContact from "./components/CreateContact"; 
 import EditContact from "./components/EditContact"; 
 
